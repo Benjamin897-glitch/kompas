e7feaf54f75ddd2ca4e51e35af71608f027b6a7b
Benjamin897-glitch <franckenbenjamin@gmail.com>
2025-01-09 11:30:07 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,5 +1,39 @@
+let graden = 0
 basic.forever(function () {
-    if (true) {
-    	
+    let de_waarde_van_het_kompas = 0
+    graden = input.compassHeading()
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    if (de_waarde_van_het_kompas > 45) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (de_waarde_van_het_kompas > 135) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (de_waarde_van_het_kompas > 225) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
     }
 })
